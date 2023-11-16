96b9af568f24760f02884d5401c62f24b961ba95
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-11-16 13:51:03 -0300

    user service

---
+++ api/services/user.service.js
@@ -0,0 +1,51 @@
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
+const { models } = require('../libs/sequelize');
+
+class UsersService {
+  async create(data) {
+    const hash = await bcrypt.hash(data.password, 10);
+    const newUser = await models.User.create({
+      ...data,
+      password: hash
+    });
+    delete newUser.dataValues.password;
+    delete newUser.dataValues.recoveryToken;
+
+    return newUser;
+  }
+
+  async find() {
+    const rta = await models.User.findAll();
+
+    return rta;
+  }
+
+  async findOne(id) {
+    const user = await models.User.findByPk(id);
+
+    if (!user)
+      throw boom.notFound('User not found');
+
+    delete user.dataValues.recoveryToken;
+
+    return user;
+  }
+
+  async update(id, changes) {
+    const user = await this.findOne(id);
+    const rta = await user.update(changes);
+
+    return rta;
+  }
+
+  async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
+
+    return { id };
+  }
+
+}
+
+module.exports = UsersService;
