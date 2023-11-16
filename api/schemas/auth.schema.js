60795c769671d63216b6e73b1ac14691f05ea798
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-11-16 12:58:02 -0300

    auth schema

---
+++ api/schemas/auth.schema.js
@@ -0,0 +1,26 @@
+const Joi = require('joi');
+
+const email = Joi.string().email();
+const password = Joi.string().min(8);
+const newPassword = Joi.string().min(8);
+const token = Joi.string();
+
+const loginAuthSchema = Joi.object({
+  email: email.required(),
+  password: password.required(),
+});
+
+const recoveryAuthSchema = Joi.object({
+  email: email.required(),
+});
+
+const changePasswordAuthSchema = Joi.object({
+  token: token.required(),
+  newPassword: newPassword.required(),
+});
+
+module.exports = {
+  loginAuthSchema,
+  recoveryAuthSchema,
+  changePasswordAuthSchema
+};
