082c4b015ecf3a5bc1e6807d782200d2e83dff3e
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-11-16 14:05:54 -0300

    pet service

---
+++ api/services/pet.service.js
@@ -0,0 +1,42 @@
+const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
+
+class PetsService {
+  async create(data) {
+    const newPet = await models.Pet.create(data);
+
+    return newPet
+  }
+
+  async find() {
+    const rta = await models.Pet.findAll();
+
+    return rta;
+  }
+
+  async findOne(id) {
+    const pet = await models.Pet.findByPk(id);
+
+    if (!pet)
+      throw boom.notFound('Pet not found');
+
+    return pet;
+  }
+
+  async update(id, changes) {
+    const pet = await this.findOne(id);
+    const rta = await pet.update(changes);
+
+    return rta;
+  }
+
+  async delete(id) {
+    const pet = await this.findOne(id);
+    await pet.destroy();
+
+    return { id };
+  }
+
+}
+
+module.exports = PetsService;
