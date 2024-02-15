fab17a1763c7e3505a99c4754e4f50bf730b539f
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-02-15 20:25:41 -0300

    add description to pets

---
+++ api/db/migrations/20240215232323-add_description_to_pets.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('pets', 'description', {
+      allowNull: true,
+      type: Sequelize.TEXT
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('pets', 'description');
+  }
+};
